ddca59ed1b07679a0e698bf395f45cfbab1a1f5d
ramesh0 <123k.ramesh@gmail.com>
2018-04-26 23:46:16 +0530

    yes good

--- Git/src/com/nisum/Dummy.java
+++ Git/src/com/nisum/Dummy.java
@@ -8,11 +8,8 @@ public class Dummy {
 		 int b;// ok approved.good
 	     int c;
 // branch cut has happend here.
-<<<<<<< HEAD
-	     //fine working..really works good.
-=======
-	     //fine working..yes cool..
->>>>>>> branch 'master' of https://github.com/ramesh0/Test.git//
+	     //fine working..really works good.Yes good
+
 
 	}
 }
