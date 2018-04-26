6d21cd4f963044f74a4dc27096f61b6f813eb8bb
ramesh0 <123k.ramesh@gmail.com>
2018-04-26 23:01:46 +0530

    something is modified

--- Git/src/com/nisum/Dummy.java
+++ Git/src/com/nisum/Dummy.java
@@ -5,11 +5,8 @@ public class Dummy {
 // added by dev2 
 	public static void main(String[] ar){
 		 int a;
-<<<<<<< HEAD
 		 int b;// ok approved.good
-=======
-		 int b;// ok approved.Thanks
->>>>>>> branch 'master' of https://github.com/ramesh0/Test.git
-		 int c;
+	 int b;// ok approved.Thanks
+	 int c;
 	}
 }
