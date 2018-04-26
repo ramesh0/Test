c3d19f8d23743fff8ac6b7e4432393a56d7459e6
ramesh0 <123k.ramesh@gmail.com>
2018-04-26 23:41:00 +0530

    my changes are final

--- Git/src/com/nisum/Dummy.java
+++ Git/src/com/nisum/Dummy.java
@@ -8,11 +8,7 @@ public class Dummy {
 		 int b;// ok approved.good
 	     int c;
 // branch cut has happend here.
-<<<<<<< HEAD
-	     //fine working..really works good.
-=======
-	     //fine working..yes cool..
->>>>>>> branch 'master' of https://github.com/ramesh0/Test.git//
+     //fine working..yes cool..
 
 	}
 }
