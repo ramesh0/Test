30708af4ad190c61e73b9d6008cf06dbbc0239d4
ramesh0 <123k.ramesh@gmail.com>
2018-04-26 22:47:00 +0530

    ok approved
    Signed-off-by: ramesh0 <123k.ramesh@gmail.com>

--- Git/src/com/nisum/Dummy.java
+++ Git/src/com/nisum/Dummy.java
@@ -1,14 +1,11 @@
 package com.nisum;
 
 public class Dummy {
-<<<<<<< HEAD
-public static void main(Sring ar[]){}
-=======
+
 // added by dev2 
 	public static void main(String[] ar){
 		 int a;
-int b;
+		 int b;// ok approved.
 		 int c;
 	}
->>>>>>> branch 'master' of https://github.com/ramesh0/Test.git
 }
