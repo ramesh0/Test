01ed65cb7451bc2f4eec0f453cb9d6d0ab77d14f
ramesh0 <123k.ramesh@gmail.com>
2018-04-26 23:07:48 +0530

    cleaned some code
    Signed-off-by: ramesh0 <123k.ramesh@gmail.com>

--- Git/src/com/nisum/Dummy.java
+++ Git/src/com/nisum/Dummy.java
@@ -4,13 +4,9 @@ public class Dummy {
 
 // added by dev2 
 	public static void main(String[] ar){
-<<<<<<< HEAD
-		
-=======
-		 int a;
+ int a;
 		 int b;// ok approved.good
-	 int b;// ok approved.Thanks
+	
 	 int c;
->>>>>>> branch 'master' of https://github.com/ramesh0/Test.git
 	}
 }
